0b9e4a230b2c1f990fba1d0f8071850430a0c478
Saschadebruijn <durpiedurp23@gmail.com>
2018-11-14 14:41:24 +0100

    Deze is helemaal af

--- afvink1.py
+++ afvink1.py
@@ -1,28 +1,26 @@
 def main():
-    try:
-        zoekwoord = str(input("Op welk zoekwoord wilt u zoeken? "))
+    zoekwoord = str(input("Op welk zoekwoord wilt u zoeken? "))
 
-        headers, seqs = lees_inhoud()
+    headers, seqs = lees_inhoud()
 
-        for i in range(len(headers)):
-            if zoekwoord in headers[i]:
-                print(headers[i])
-                print(seqs[i])
-                checkrna = is_dna(seqs[i])
-                print("Is het RNA", checkrna)
-                knipt(seqs[i])
-        print("\n")
-    except IOError:
-        print("Kan het bestand niet vinden.")
-    except ZeroDivisionError:
-        print("Kan getal niet delen door 0.")
-    except ValueError:
-        print("Er zit een verkeerd getal in.")
-    except:
-        print("Onbekende error.")
+    for i in range(len(headers)):
+        if zoekwoord in headers[i]:
+            print(headers[i])
+            print(seqs[i])
+            checkrna = is_dna(seqs[i])
+            print("Is het RNA", checkrna)
+            knipt(seqs[i])
+    print("\n")
+
 
 
 def lees_inhoud():
+    """Deze functie leest het bestand en split de inhoud in 2 lijsten
+    input: Bestand
+
+    output:
+    headers --> lijst van headers
+    seq --> lijst van sequenties"""
     try:
         bestand = open("/home/sasnee/Documents/School Bio-informatica/Informatica/Blok 1/Week 6/2testseq.txt", "r")
         headers = []
@@ -49,10 +47,14 @@ def lees_inhoud():
         print("Onbekende error.")
 
 
-"""Deze functie leest het bestand"""
+
 
 
 def is_dna(seq):
+    """Deze functie checked of de sequentie een RNA- of DNA sequentie is
+    input: seq - lijst van sequenties
+
+    output: boolean"""
     try:
         print("\n")
         rna = True
@@ -71,15 +73,20 @@ def is_dna(seq):
         print("Onbekende error.")
 
 
-"""Deze functie checked of de sequentie een RNA- of DNA sequentie is"""
+
 
 
 def knipt(seq):
+    """Deze functie kijkt waar enzymen knippen op de sequentie, ook laat het zien welke enzymen knippen
+    input: seq, lijst van sequenties,
+        bestand met enzymen
+
+    output: print statement dat laat zien welk enzym knipt"""
     try:
         print("\n")
         enzymen = open(
-            "/home/sasnee/Documents/School Bio-informatica/github/1718-owe1a-afvinkopdracht5-Saschadebruijn/enzymen.txt",
-            "r")
+                "/home/sasnee/Documents/School Bio-informatica/github/Blok 1/1718-owe1a-afvinkopdracht5-Saschadebruijn/enzymen.txt",
+                "r")
         for line in enzymen:
             naam, code = line.split()
             code = code.replace("^", "")          ##opent een bestand met enzymen die dan wordt gebruikt bij het checken
@@ -97,6 +104,6 @@ def knipt(seq):
         print("Onbekende error.")
 
 
-"""Deze functie kijkt waar enzymen knippen op de sequentie, ook laat het zien welke enzymen knippen"""
+
 
 main()
